6e1b0c7d65cec7f034f213dae15953cd5e43b6b6
Brett Camarda <bcamarda@gmail.com>
2012-07-29 08:06:55 -0700

    fixed some migrations for heroku

--- db/migrate/20120729061711_remove_columns_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnsFromUsers < ActiveRecord::Migration
-  def up
-  	remove_column :users, :role, :string
-  	remove_column :users, :twitter_handle, :string
-  end
-
-  def down
-  end
-end
