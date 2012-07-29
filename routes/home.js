afdcb1c7b6e685867b835a6108b013afc493d66d
Raynos <raynos2@gmail.com>
2012-07-29 13:22:52 -0700

    no more node

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-var main = require("..")
-    , template = main.template
-    , redirect = require("redirecter")
-
-module.exports = home
-
-function home(req, res)  {
-    redirect(req, res, "/public/index.html")
-}
