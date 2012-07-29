c558372b0d87eb963e245015faa3060d35f4d826
Raynos <raynos2@gmail.com>
2012-07-28 23:44:47 -0700

    bootstrap and public index.html file

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ var http = require("http")
     , router = new routes.Router()
     , path = require("path")
     , errorPage = require("error-page")
-    , $static = require("routil-static")({
-        uri: path.join(__dirname, "static")
-    })
+    , send = require("send")
+    , url = require("url")
 
 Templar.loadFolder('./templates')
 
@@ -16,16 +15,32 @@ module.exports = {
     template: template
 }
 
-router.addRoute("/static/*?", $static)
+router.addRoute("/public/*?", function (req, res) {
+    function error(err) {
+        res.statusCode = err.status || 500;
+        res.end(err.message);
+    }
+
+    // your custom directory handling logic:
+    function redirect() {
+        res.statusCode = 301;
+        res.setHeader('Location', req.url + '/');
+        res.end('Redirecting to ' + req.url + '/');
+    }
+
+    // transfer arbitrary files from within
+    // /www/example.com/public/*
+    send(req, url.parse(req.url).pathname)
+        .root(__dirname)
+        .on('error', error)
+        .on('directory', redirect)
+        .pipe(res);
+})
 router.addRoute("/", require("./routes/home"))
 router.addRoute("/issues", require("./routes/issues"))
 router.addRoute("/issues/:issueId", require("./routes/issuesItem"))
 router.addRoute("/user/signup", require("./routes/user/signup"))
 router.addRoute("/user", require("./routes/user/main.js"))
-
-$static.load("static/**/*.*", function (err) {
-    throw err
-})
 
 server.on("request", function (req, res) {
     var route = router.match(req.url)
