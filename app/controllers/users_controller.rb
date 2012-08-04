f6e4e5c1bfcff3bba13715bf9f35aee008d99d9c
mfucci <mfucci@192.168.0.198>
2012-08-04 15:56:24 -0700

    Add upgrade to champion (not working for now)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,9 @@
 class UsersController < ApplicationController
   def update
     @user = current_user
-    @user.rep_status = true
     @user.save
     flash[:messages] = "Your submission has been edited!"
     redirect_to root_path
-  end
-
-  def upgrade
-    @user = current_user
-    render "users/upgrade"
-  end
-
-  def edit
-    if current_user.rep_status == true
-      @user = current_user
-    else
-      render "users_path"
-    end
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name, :location
+  validates_presence_of :name
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :bio, :twitter_handle, :location, :rep_status, :name
